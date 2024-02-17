2649856f2e5b6caf890f266d9e53e48ceccd41d7
PASUPULETI NAVEEN KUMAR <pasupuletin28@gmail.com>
2024-02-17 11:22:59 +0530

    First commit

---
+++ Pattern/src/com/kodnest/pattern/P2.java
@@ -0,0 +1,11 @@
+package com.kodnest.pattern;
+
+public class P2 {
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++)
+		{
+			System.out.print("*"+" ");
+		}
+	}
+
+}
---
+++ Pattern/src/com/kodnest/pattern/P3.java
@@ -0,0 +1,9 @@
+package com.kodnest.pattern;
+
+public class P3 {
+	public static void main(String[] args) {
+		System.out.println("*");
+		
+	}
+
+}
